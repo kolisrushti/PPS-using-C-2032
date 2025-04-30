7ae9ea5ae013aefc4fc0001edfe9d9a99519e7e8
kolisrushti <srushtikoli932@gmail.com>
2025-04-30 12:51:24 +0530

    Add two numbers ( user input)

---
+++ program to add two numbers (user inputs).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf ("Enter first Number");
+    scanf("%d and num1");
+    printf("Enter second number");
+    scanf("%d and num2");
+    sum=num1+num2;
+    printf("sum of %d and %d is %d num1,num2,sum");
+    return 0;
+}  
