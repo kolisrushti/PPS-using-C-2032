0e867c0c9fe5ac913c4fcaba6a02b788e7aa1a5b
kolisrushti <srushtikoli932@gmail.com>
2025-04-30 16:38:58 +0530

    Student marks and pass condition

---
+++ Student marks and pass condition .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main() 
+ { 
+    int marks;
+    printf("Enter marks;\t");
+    scanf("%d and marks ");
+    if(marks>=40)
+ {  printf("pass");
+ }
+    printf("End of program ");
+    return 0;
+ }
