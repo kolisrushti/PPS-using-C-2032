332ef8aedb077819d717ec7a5f50d9897221afa0
kolisrushti <srushtikoli932@gmail.com>
2025-06-05 13:05:24 +0530

    Marks of students

---
+++ marks of students .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+   int marks[5],i;
+   for(i=0;i<5;i++)
+   {
+      printf ("Enter marks of %d student,i+l");
+      scanf ("%d",& marks [i]);
+   }
+   for (i=0;i<5;i++)
+   {
+       printf ("marks of %d are %d",i+1,marks[i]);
+       
+   }
+   return 0;
+}
+    
